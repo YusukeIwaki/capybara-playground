3cc3b200f60d9b8c59490d233b856bca6c2a0f05
YusukeIwaki <iwaki@i3-systems.com>
2021-05-09 23:14:16 +0900

    accept_modal testing

--- app/test_app.rb
+++ app/test_app.rb
@@ -2,4 +2,47 @@ require 'sinatra/base'
 
 class TestApp < Sinatra::Base
   get('/') { '<h1>It works!</h1>' }
+
+  get('/alert') {
+    <<~HTML
+    <html>
+    <head>
+    <script type="text/javascript">
+    window.addEventListener('DOMContentLoaded', () => {
+      document.querySelector('button').addEventListener('click', (evt) => {
+        alert('Hoge');
+        document.getElementById('result').innerText = `clicked`;
+      }, false);
+    }, false);
+    </script>
+    </head>
+    <body>
+      <button>Click me</button>
+      <span id="result"></span>
+    </body>
+    </html>
+    HTML
+  }
+
+  get('/confirm_twice') {
+    <<~HTML
+    <html>
+    <head>
+    <script type="text/javascript">
+    window.addEventListener('DOMContentLoaded', () => {
+      document.querySelector('button').addEventListener('click', (evt) => {
+        if (confirm('Are you sure?') && confirm('Are you really sure?')) {
+          document.getElementById('result').innerText = `clicked`;
+        }
+      }, false);
+    }, false);
+    </script>
+    </head>
+    <body>
+      <button>Click me</button>
+      <span id="result"></span>
+    </body>
+    </html>
+    HTML
+  }
 end
--- spec/feature/example_spec.rb
+++ spec/feature/example_spec.rb
@@ -5,4 +5,54 @@ RSpec.describe 'example' do
     visit '/'
     expect(page).to have_content('It works!')
   end
+
+  it 'fails on dialog shown', pending: true do
+    visit '/alert'
+    find('button').click # ここでおちる
+    expect(page).to have_content('clicked')
+  end
+
+  it 'passes with accept_alert' do
+    visit '/alert'
+    accept_alert do
+      find('button').click
+    end
+    expect(page).to have_content('clicked')
+  end
+
+  it 'fails if alert is not shown' do
+    visit '/alert'
+    expect {
+      accept_alert do
+        # do nothing
+      end
+    }.to raise_error(Capybara::ModalNotFound)
+  end
+
+  it 'fails if alert is not shown' do
+    visit '/alert'
+    expect {
+      dismiss_confirm do
+        # do nothing
+      end
+    }.to raise_error(Capybara::ModalNotFound)
+  end
+
+  it 'fails on 2 dialog shown', pending: true do
+    visit '/confirm_twice'
+    accept_confirm do
+      find('button').click
+    end
+    expect(page).to have_content('clicked')
+  end
+
+  it 'passes with nested handler on 2 dialog shown' do
+    visit '/confirm_twice'
+    accept_confirm do
+      accept_confirm do
+        find('button').click
+      end
+    end
+    expect(page).to have_content('clicked')
+  end
 end
